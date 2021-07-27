f83c8ec118af42384bb822e81c4195009673b476
AngelicaJH <expotextil.flor@gmail.com>
2021-07-27 18:27:03 -0500

    Se muestran los datos de la peli

--- src/main.js
+++ src/main.js
@@ -1,6 +1,47 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import {example} from './data.js';
 
-console.log(example, data);
+import data from './data/ghibli/ghibli.js';
+
+
+//Crea las tarjetas y muestra la data de la pelicula. 
+window.onload=function print(){
+const container = document.getElementById('card');
+const allMovies= data.films.map(movie => 
+    `<div id="cardMovie">
+    <img id="imgMovie" src="${movie.poster}">
+    <h4 id="titleMovie">${movie.title}</h4>
+    <buttom id="bntViewMovie">More Detalls</button>
+    </div>
+    `
+); container.innerHTML=allMovies.join('');
+}
+
+
+/*
+data.films.forEach(movie=> {
+    let cardMovie = document.createElement("div");         // Create a <div> node
+    let imgMovi = document.createElement("img");            //Cracion de imagen
+    let btnDetalls = document.createElement("button");
+    cardMovie.id="cardMovie";
+    imgMovi.id="imgMovie";
+    
+    imgMovi.src=`${movie.poster}`;
+    
+    
+    let movieTitle = document.createTextNode(movie.title);       // Create a text node
+    
+    cardMovie.appendChild(movieTitle);                              // Append the text to <P>
+    cardMovie.appendChild(imgMovi);
+     cardMovie.appendChild(btnDetalls);
+
+
+    document.getElementById("card").appendChild(cardMovie);                    
+    
+    //document.getElementById("card").appendChild(imgMovi);
+ 
+});*/
+
+
+
+
+
